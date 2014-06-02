e2051f29047a9ce133cda84d8e7962e49ea6a743
George Balatsouras <gbalats@gmail.com>
2014-06-02 17:33:23 +0300

    Adding javadoc for `jphantom.Types'.

--- src/jphantom/Types.java
+++ src/jphantom/Types.java
@@ -3,15 +3,31 @@ package jphantom;
 import java.util.*;
 import org.objectweb.asm.Type;
 
-public interface Types {
-
+/**
+ * Constant interface (anti)pattern for some commonly used ASM
+ * types.
+ */
+public interface Types
+{
+    /** The ASM type of {@link java.lang.Object}. */
     Type OBJECT = Type.getType(Object.class);
+
+
+    /** The ASM type of {@link java.lang.Throwable}. */
     Type THROWABLE = Type.getType(Throwable.class);
+
+
+    /** The ASM null type. This is the type of the expression null. */
     Type NULL_TYPE = Type.getObjectType("null");
 
-    // Array Interfaces
 
-    Set<Type> ARRAY_INTERFACES = 
-        new HashSet<>(Arrays.asList(Type.getType(Cloneable.class), 
+    /**
+     * The set of interfaces implemented by arrays, as ASM types.
+     *
+     * It consists of the {@link java.lang.Cloneable} and {@link
+     * java.io.Serializable} interfaces.
+     */
+    Set<Type> ARRAY_INTERFACES =
+        new HashSet<>(Arrays.asList(Type.getType(Cloneable.class),
                                     Type.getType(java.io.Serializable.class)));
 }
