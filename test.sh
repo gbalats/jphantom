089149a1dc59fdb2ca13930dba948a1d76c8f226
George Balatsouras <gbalats@gmail.com>
2016-06-02 15:43:41 +0300

    Remove some useless files

--- test.sh
+++
@@ -1,7 +0,0 @@
-# test.sh
-#
-#!/bin/bash
-
-java -jar build/jar/jphantom.jar ../.old/asm/complement/pernasvip-ant/build/jar/itGen.jar --save-class-files
-echo 'Differences in output: '
-diff -r out/phantoms/org/ ../.old/asm/complement/out/phantoms/org/
